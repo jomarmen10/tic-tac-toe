be07c35d1ed720674881ec58812ab2dce2265317
Jomar Mendoza <jmm21@Jomars-MBP.home>
2019-07-27 13:51:35 -0700

    added the board function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+let board
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+function init(){
+  board = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+  ]
+}
