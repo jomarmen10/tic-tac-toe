d6738a4bc564c154bef0d61c5bdd1d237a8e9e89
Jomar Mendoza <jmm21@Jomars-MBP.home>
2019-07-28 16:15:32 -0700

    added the const variables

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 /*----- constants -----*/
+const moves = {
+  "0": "white",
+  "1": "red",
+  "-1": "blue"
+}
 /*----- app's state (variables) -----*/
 let board
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+init();
+
 function init(){
   board = [
     [0,0,0],
     [0,0,0],
     [0,0,0]
-  ]
+  ];
+
 }
